4f07fe40c434e1330bee53a2d6d1759d4c1d968e
navinprasadk <NA351942@wipro.com>
2018-03-27 21:01:51 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
+// const express = require("express"),
+//   nodemailer = require("nodemailer"),
+//   app = express(),
+//   PORT = process.env.PORT || 8080,
+//   server = require("http").Server(app);
+
+// app.use(express.static("./../"));
+
+// server.listen(PORT, (err, res) => {
+//   if (err) {
+//     console.log("error in server - > ", err);
+//   } else {
+//     console.log("Server up and listening on port ", PORT);
+//   }
+// });
+
 const express = require("express"),
   nodemailer = require("nodemailer"),
   app = express(),
   PORT = process.env.PORT || 8080,
   server = require("http").Server(app);
-
-app.use(express.static("./../"));
 
 server.listen(PORT, (err, res) => {
   if (err) {
@@ -14,28 +28,23 @@ server.listen(PORT, (err, res) => {
   }
 });
 
-// const express = require('express')
-//     , app = express()
-//     , server = http.createServer(app)
-//     , port = process.env.port || 1100;
-//
-// // allowing CORS control to the app server ---->
+// allowing CORS control to the app server ---->
 // app.use(function(req, res, next) {
 //  	res.header("Access-Control-Allow-Origin", "*");
 //  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //  	next();
 //  	});
-//
-// // serving up static files to the server Eg.: img, html, css, js files ---->
-// app.use(express.static('./../'));
-//
-// // server routes ---->
-// app.use('/',(req,res,next)=>{
-//   console.log('inside routes');
-//   next();
-// });
-//
-// // serving up the app server to the environment port --->
+
+// serving up static files to the server Eg.: img, html, css, js files ---->
+app.use(express.static("./../client"));
+
+// server routes ---->
+app.use("/", (req, res, next) => {
+  console.log("inside routes");
+  next();
+});
+
+// serving up the app server to the environment port --->
 // server.listen(port, (err, res)=>{
 //   if (err) {
 //     console.log('err in serving up the app server - > ',err);
